661de8736361f3dce28b37105e45e9943921304e
Cawi <cawicaancornelio@gmail.com>
2014-08-25 12:51:29 +0800

    Initial Commit

---
+++ OneSolutionV3/OneSolutionV3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OneSolutionV3.MvcApplication" Language="C#" %>
---
+++ OneSolutionV3/OneSolutionV3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OneSolutionV3.Startup))]
+namespace OneSolutionV3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
